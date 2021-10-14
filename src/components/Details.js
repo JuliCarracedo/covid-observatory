3661f0f7c918fde1c8999bca6221f05473cf1809
JuliCarracedo <julicarra@hotmail.com>
2021-10-14 17:10:54 -0300

    Patch styles and add test folder

--- src/components/Details.js
+++ src/components/Details.js
@@ -41,7 +41,7 @@ const Details = () => {
       <p className="subtitle">Covid-19 cases by region</p>
       {regions.map((region) => (
         <div className="region-card" key={region.name}>
-          <h3>{region.name}</h3>
+          <h3 className="region-title">{region.name}</h3>
           <div className="count-container">
             <p className="align-right">{region.count}</p>
             <svg className="card-svg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm1 17v-4h-8v-2h8v-4l6 5-6 5z" /></svg>
